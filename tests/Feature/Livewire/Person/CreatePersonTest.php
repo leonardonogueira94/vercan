86112ee1932de842cdee3968dcd4716ee660836e
Leonardo SIlva <leonardo.silva@goeyewear.com.br>
2023-07-06 12:48:20 -0300

    .

--- tests/Feature/Livewire/Person/CreatePersonTest.php
+++ tests/Feature/Livewire/Person/CreatePersonTest.php
@@ -21,5 +21,41 @@ class CreatePersonTest extends TestCase
      */
     protected $seed = true;
 
-    
+    /**
+     * @test
+     * @medium
+     * @dataProvider cepProvider
+     * @covers \App\Http\Livewire\CreatePerson::updated
+     * @covers \App\Http\Livewire\CreatePerson::fillAddress
+     * @covers \App\Services\CepService::getAddressDataByCep
+     * @covers \App\Services\CepService::getAddressDataMap
+     */
+    public function if_address_gets_filled_when_cep_is_filled(string $cep, string $expectedCompanyName)
+    {
+        $person = Person::first();
+        
+        $component = Livewire::test(CreatePerson::class, ['person' => $person])
+        ->set('cep', $cep)
+        ->assertNotSet('address', $person->address->address)
+        ->assertSet('address', $expectedCompanyName);
+    }
+
+    /**
+     * @test
+     * @medium
+     * @dataProvider cnpjProvider
+     * @covers \App\Http\Livewire\CreatePerson::updated
+     * @covers \App\Services\ReceitaService::getLegalPersonData
+     * @covers \App\Services\ReceitaService::getLegalPersonDataMap
+     * @covers \App\Http\Livewire\CreatePerson::fillPersonData
+     */
+    public function if_company_name_gets_filled_when_cnpj_is_filled(string $cnpj, string $expectedCompanyName)
+    {
+        $person = Person::first();
+        
+        $component = Livewire::test(CreatePerson::class, ['person' => $person])
+        ->set('cnpj', $cnpj)
+        ->assertNotSet('companyName', $person->company_name)
+        ->assertSet('companyName', $expectedCompanyName);
+    }
 }
