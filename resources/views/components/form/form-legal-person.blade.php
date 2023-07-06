fe26a772589fd4ee4c46b274f704c54fd206f045
Leonardo SIlva <leonardo.silva@goeyewear.com.br>
2023-07-06 02:25:04 -0300

    .

--- resources/views/components/form/form-legal-person.blade.php
+++ resources/views/components/form/form-legal-person.blade.php
@@ -2,17 +2,17 @@
     <div class="row">
         <div class="col-lg-3 col-12">
             <label class="control-label">CNPJ</label><sup>•</sup>
-            <input wire:model.defer="cnpj" class="form-control form-control-sm cnpj" required @if($this->disableInputs) disabled @endif>
+            <input wire:model.defer="cnpj" value="{{ $this->cnpj }}" class="form-control form-control-sm cnpj" required @if($this->disableInputs) disabled @endif>
             @error('cnpj') <span class="error">{{ $message }}</span>@enderror
         </div>
         <div class="col-lg-6 col-12">
             <label class="control-label">Razão Social</label><sup>•</sup>
-            <input wire:model.debounce.500ms="companyName" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
+            <input wire:model.debounce.500ms="companyName" value="{{ $this->companyName }}" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
             @error('companyName') <span class="error">{{ $message }}</span>@enderror
         </div>
         <div class="col-lg-3 col-12">
             <label class="control-label">Nome Fantasia</label><sup>•</sup>
-            <input wire:model.debounce.500ms="tradingName" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
+            <input wire:model.debounce.500ms="tradingName" value="{{ $this->tradingName }}" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
             @error('tradingName') <span class="error">{{ $message }}</span>@enderror
         </div>
     </div>
@@ -20,7 +20,7 @@
     <div class="row">
         <div class="col-lg-3 col-12">
             <label class="control-label">Indicador de Inscrição Estadual</label><sup>•</sup>
-            <select wire:model.debounce.500ms="stateRegistrationCategory" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
+            <select wire:model.debounce.500ms="stateRegistrationCategory" value="{{ $this->stateRegistrationCategory }}" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
                 <option hidden>Selecione</option>
                 @foreach(App\Enums\Person\StateRegistrationCategory::cases() as $case)
                     <option value="{{ $case }}" @if($this->stateRegistrationCategory == $case->value) {{ 'selected' }} @endif>{{ $case->label() }}</option>
@@ -31,18 +31,18 @@
         @if($this->type == App\Enums\Person\PersonType::JURIDICA->value)
             <div class="col-lg-3 col-12">
                 <label class="control-label">Inscrição Estadual</label> @if(App\Enums\Person\StateRegistrationCategory::tryFrom($this->stateRegistrationCategory)->required())<sup>•</sup>@endif
-                <input wire:model.debounce.500ms="ie" class="form-control form-control-sm" @if(App\Enums\Person\StateRegistrationCategory::tryFrom($this->stateRegistrationCategory)->required()) {{ 'required' }} @else {{ 'disabled '}} @endif @if($this->disableInputs) disabled @endif>
+                <input wire:model.debounce.500ms="ie" value="{{ $this->ie }}" class="form-control form-control-sm" @if(App\Enums\Person\StateRegistrationCategory::tryFrom($this->stateRegistrationCategory)->required()) {{ 'required' }} @else {{ 'disabled '}} @endif @if($this->disableInputs) disabled @endif>
                 @error('ie') <span class="error">{{ $message }}</span>@enderror
             </div>
         @endif
         <div class="col-lg-3 col-12">
             <label class="control-label">Inscrição Municipal</label>
-            <input wire:model.debounce.500ms="im" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
+            <input wire:model.debounce.500ms="im" value="{{ $this->im }}" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
             @error('im') <span class="error">{{ $message }}</span>@enderror
         </div>
         <div class="col-lg-3 col-12">
             <label class="control-label">Situação CNPJ</label>
-            <input wire:model.debounce.500ms="cnpjStatus" class="form-control form-control-sm" id="cnpj" required @if($this->disableInputs) disabled @endif>
+            <input wire:model.debounce.500ms="cnpjStatus" value="{{ $this->cnpjStatus }}" class="form-control form-control-sm" id="cnpj" required @if($this->disableInputs) disabled @endif>
             @error('cnpjStatus') <span class="error">{{ $message }}</span>@enderror
         </div>
     </div>
--- resources/views/components/form/form-natural-person.blade.php
+++ resources/views/components/form/form-natural-person.blade.php
@@ -2,29 +2,29 @@
     <div class="row">
         <div class="col-lg-3 col-12">
             <label class="control-label">CPF</label><sup>•</sup>
-            <input wire:model.defer="cpf" class="form-control form-control-sm cpf" required @if($this->disableInputs) disabled @endif>
+            <input wire:model.defer="cpf" value="{{ $this->cpf }}" class="form-control form-control-sm cpf" required @if($this->disableInputs) disabled @endif>
             @error('cpf') <span class="error">{{ $message }}</span>@enderror
         </div>
         <div class="col-lg-6 col-12">
             <label class="control-label">Nome</label><sup>•</sup>
-            <input wire:model.debounce.500ms="name" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
+            <input wire:model.debounce.500ms="name" value="{{ $this->name }}" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
             @error('name') <span class="error">{{ $message }}</span>@enderror
         </div>
         <div class="col-lg-3 col-12">
             <label class="control-label">Apelido</label>
-            <input wire:model.debounce.500ms="alias" class="form-control form-control-sm" @if($this->disableInputs) disabled @endif>
+            <input wire:model.debounce.500ms="alias" value="{{ $this->alias }}" class="form-control form-control-sm" @if($this->disableInputs) disabled @endif>
             @error('alias') <span class="error">{{ $message }}</span>@enderror
         </div>
     </div>
     <div class="row">
         <div class="col-lg-3 col-12">
             <label class="control-label">RG</label><sup>•</sup>
-            <input wire:model.debounce.500ms="rg" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
+            <input wire:model.debounce.500ms="rg" value="{{ $this->rg }}" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
             @error('rg') <span class="error">{{ $message }}</span>@enderror
         </div>
         <div class="col-lg-3 col-12">
             <label class="control-label">Ativo</label><sup>•</sup>
-            <select wire:model.debounce.500ms="personStatus" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
+            <select wire:model.debounce.500ms="personStatus" value="{{ $this->personStatus }}" class="form-control form-control-sm" required @if($this->disableInputs) disabled @endif>
                 <option hidden>Selecione</option>
                 @foreach(App\Enums\Person\PersonStatus::cases() as $case)
                     <option value="{{ $case->value }}" @if($this->personStatus == $case->value) selected @endif>{{ $case->label() }}</option>
